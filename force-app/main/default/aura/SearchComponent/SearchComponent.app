a1604554da33f02501712ba743a862f3e837cea4
Karla Bello <karlabello1024006@gmail.com>
2022-06-16 20:31:15 -0400

    searchBarProgress3

--- force-app/main/default/aura/SearchComponent/SearchComponent.app
+++ force-app/main/default/aura/SearchComponent/SearchComponent.app
@@ -1,4 +1,4 @@
 <aura:application extends="force:slds">
-    <c:searchBarMovies1/>
+    <c:searchBarMovies/>
 
 </aura:application>	
--- force-app/main/default/lwc/searchBarMovies/searchBarMovies.js
+++ force-app/main/default/lwc/searchBarMovies/searchBarMovies.js
@@ -1,8 +1,26 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track,wire,api} from 'lwc';
 import displayMovieRecords from '@salesforce/apex/MoviesSearchBarController.displayMovieRecords';
+import Movie from '../movie/movie';
+
 
 export default class SearchBarMovies extends LightningElement {
-    @track movMovieTitle;
+    @api movMovieTitle;
+    @track records; 
+    @track error;
+    @api recordId;
+        
+    @wire (displayConRecords,{searchkey:'$movMovieTitle'})   
+
+    wireMovies({error,data}){
+        if (data){
+            this.records=data;
+            this.error=error;    
+        }
+        else{
+            this.error=error;
+            this.error=undefined;
+        }
+    }
     handleChange(event){
         this.movMovieTitle=event.target.value;
         console.log('MovieTitle'+this.movMovieTitle);
