f3831d32f26af8697809f99dc91f82473fb684be
ErickJValdez <erick.valdez.vargas@gmail.com>
2022-06-17 14:59:17 -0400

    Error for deployment corrected

--- force-app/main/default/lwc/searchBarMovies/searchBarMovies.js
+++ force-app/main/default/lwc/searchBarMovies/searchBarMovies.js
@@ -1,24 +1,14 @@
-<<<<<<< HEAD
-import { LightningElement, track, wire } from 'lwc';
-=======
-import { LightningElement, track,wire,api} from 'lwc';
->>>>>>> 9ba4e41096e531c9741f7b80ef030eb378d023d0
-import displayMovieRecords from '@salesforce/apex/MoviesSearchBarController.displayMovieRecords';
-import Movie from '../movie/movie';
+import { LightningElement, api, track, wire } from 'lwc';
 
+import displayMovieRecords from '@salesforce/apex/MoviesSearchBarController.displayMovieRecords';
 
 export default class SearchBarMovies extends LightningElement {
-<<<<<<< HEAD
-    @wire(displayMovieRecords) MoviesTitles;
-
-    @track movMovieTitle;
-=======
     @api movMovieTitle;
     @track records; 
     @track error;
     @api recordId;
         
-    @wire (displayConRecords,{searchkey:'$movMovieTitle'})   
+    @wire (displayMovieRecords,{searchkey:'$movMovieTitle'})   
 
     wireMovies({error,data}){
         if (data){
@@ -30,7 +20,6 @@ export default class SearchBarMovies extends LightningElement {
             this.error=undefined;
         }
     }
->>>>>>> 9ba4e41096e531c9741f7b80ef030eb378d023d0
     handleChange(event){
         this.movMovieTitle=event.target.value;
         console.log('MovieTitle'+this.movMovieTitle);
