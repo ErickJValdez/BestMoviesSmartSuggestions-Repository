11fb025cc1ac2e7f267ea8abc097c0eabb8dbff5
ErickJValdez <erick.valdez.vargas@gmail.com>
2022-06-20 11:50:30 -0400

    Solving top 10  issues

--- force-app/main/default/lwc/top10movies/top10movies.js
+++ force-app/main/default/lwc/top10movies/top10movies.js
@@ -1,14 +1,14 @@
 import {LightningElement, wire  } from "lwc";
 import getTopMovies from '@salesforce/apex/TopController.getTopMovies';
-import hasAccessUI from '@salesforce/customPermission/MoviePermission';
+//import hasAccessUI from '@salesforce/customPermission/MoviePermission';
 
 export default class TopMovies extends LightningElement{
    
     @wire(getTopMovies) movie__c;
 
-    get isUIAccessible(){
+ /*   get isUIAccessible(){
         return hasAccessUI;
-    }
+    }*/
 
     
 }
