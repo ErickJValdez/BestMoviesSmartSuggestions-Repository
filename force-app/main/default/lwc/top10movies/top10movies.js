47a6a6e15556bae32d7a348ef0e3ffb73b635b44
Darianny Escoboza <dariannyn19@gmail.com>
2022-06-22 16:56:14 -0400

    TopMovies

--- force-app/main/default/lwc/top10movies/top10movies.js
+++ force-app/main/default/lwc/top10movies/top10movies.js
@@ -1,14 +1,11 @@
 import {LightningElement, wire  } from "lwc";
 import getTopMovies from '@salesforce/apex/TopController.getTopMovies';
-//import hasAccessUI from '@salesforce/customPermission/MoviePermission';
+
+
 
 export default class TopMovies extends LightningElement{
    
     @wire(getTopMovies) movie__c;
-
- /*   get isUIAccessible(){
-        return hasAccessUI;
-    }*/
 
     
 }
