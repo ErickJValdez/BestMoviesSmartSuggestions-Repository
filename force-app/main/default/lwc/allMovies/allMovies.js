d239dcef91b90393b6376dd01af729607a441d6f
Darianny Escoboza <dariannyn19@gmail.com>
2022-06-24 09:39:47 -0400

    All movies LWC

---
+++ force-app/main/default/lwc/allMovies/allMovies.js
@@ -0,0 +1,7 @@
+import { LightningElement, wire } from 'lwc';
+import getAllMovies from '@salesforce/apex/allMoviesController.getAllMovies'
+
+export default class AllMovies extends LightningElement {
+
+    @wire(getAllMovies) movies;
+}
