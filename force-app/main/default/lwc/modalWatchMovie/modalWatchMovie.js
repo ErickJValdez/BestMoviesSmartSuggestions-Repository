e1a68fcd596f03900dd3ba5e858aaadc79f88380
ErickJValdez <erick.valdez.vargas@gmail.com>
2022-06-24 10:54:51 -0400

    Movie genres added to modal

--- force-app/main/default/lwc/modalWatchMovie/modalWatchMovie.js
+++ force-app/main/default/lwc/modalWatchMovie/modalWatchMovie.js
@@ -1,10 +1,26 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import prefferedContactGenres from '@salesforce/apex/CategoryMovieController.prefferedContactGenres';
+
 
 export default class ModalWatchMovie extends LightningElement {
 
     showModalWatchMovie = false;
     showRank = false;
     movieValue;
+    movieGenres;
+
+    @wire(prefferedContactGenres,{movieId:'$movieValue.Id'})
+    movieGenre({ error, data }) {
+      if(data){
+        this.movieGenres = data;
+        console.log(data);
+        return 
+      }
+      console.log(error);
+    }
+
+
+
 
   @api show(evt) {
     this.showModalWatchMovie = true;
