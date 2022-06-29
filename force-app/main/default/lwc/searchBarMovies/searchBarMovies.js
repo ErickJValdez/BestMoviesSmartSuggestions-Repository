28cb809be0df7b9d4c195a830d603e92c2e09779
Karla Bello <karlabello1024006@gmail.com>
2022-06-29 15:08:49 -0400

    add show modal on search

--- force-app/main/default/lwc/searchBarMovies/searchBarMovies.js
+++ force-app/main/default/lwc/searchBarMovies/searchBarMovies.js
@@ -1,18 +1,32 @@
-import { LightningElement, api, track, wire } from 'lwc';
+import { LightningElement } from 'lwc';
 
 import search from '@salesforce/apex/MoviesSearchBarController.search';
 
 export default class SearchBarMovies extends LightningElement {
+    results = [];
+    
     handleSearch(event) {
         const lookupElement = event.target;
         search(event.detail)
             .then(results => {
+                this.results = results;
                 lookupElement.setSearchResults(results);
             })
             .catch(error => {
                 console.log('The movie wasn\'t found' + error);
             });
     }
-    
 
+    handleSelectionChange(event) {
+        const selectedId = event.detail?.[0];
+
+        if(!selectedId) {
+            return;
+        }
+        
+        const selectedMovie = this.results.find(movie => movie.id === selectedId)?.payload;
+
+        const modal = this.template.querySelector("c-modal-Watch-Movie");
+        modal.show( selectedMovie );
+    }
 }
