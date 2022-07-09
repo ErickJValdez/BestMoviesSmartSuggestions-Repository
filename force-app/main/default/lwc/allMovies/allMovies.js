15c4c5e8e621095b19937e13b5d36bb2aff1f302
Darianny Escoboza <dariannyn19@gmail.com>
2022-07-08 20:21:26 -0400

    all movies resize

--- force-app/main/default/lwc/allMovies/allMovies.js
+++ force-app/main/default/lwc/allMovies/allMovies.js
@@ -4,4 +4,17 @@ import getAllMovies from '@salesforce/apex/allMoviesController.getAllMovies'
 
 export default class AllMovies extends LightningElement {
     @wire(getAllMovies) movies;
+    handlePageChange(){
+        getAllMovies().then(result => {
+            console.log("Prueba");
+        })
+    }
+    
+    handleNextPage(event){
+        if(this.pageNumber < this.totalPageCount){
+            this.pageNumber = this.pageNumber + 1;
+        }
+        console.log("Current Page: " +this.pageNumber);
+        this.handlePageChange();
+    }
 }
